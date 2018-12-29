9f0620a537ac2dcdbb4feb8d1a2c949339a6327f
Lisa Kim <lisakeem@outlook.com>
2018-12-28 16:57:27 -0800

    Remove password length constraint

--- int/hwsc-user-svc/0_initial_schema.up.sql
+++ int/hwsc-user-svc/0_initial_schema.up.sql
@@ -23,9 +23,7 @@ CREATE TABLE user_account
   first_name        USER_NAME,
   last_name         USER_NAME,
   email             VARCHAR(320) NOT NULL UNIQUE,
-  password          VARCHAR(20) NOT NULL
-                    CONSTRAINT password_length
-                    CHECK(length(password) > 7),
+  password          VARCHAR(60) NOT NULL,
   organization      TEXT,
   create_timestamp  TIMESTAMP NOT NULL,
   is_verified       BOOLEAN NOT NULL DEFAULT FALSE
