ee33df2f1c1466266be6d35a7c95c5aa8e9addcf
CJ Faraon <faraonc@outlook.com>
2018-12-30 00:37:43 -0800

    Add restore procedure for dev-document MongoDB (#5)

--- backup_mongodb_server.sh
+++ backup_mongodb_server.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
 mkdir -p backup/unit-test
-mongodump --host $mongo_host:$mongo_port --db dev-user --collection dev-user -u $mongo_user -p $mongo_key  --ssl --sslAllowInvalidCertificates -o backup/unit-test
 mongodump --host $mongo_host:$mongo_port --db dev-document --collection dev-document -u $mongo_user -p $mongo_key  --ssl --sslAllowInvalidCertificates -o backup/unit-test
