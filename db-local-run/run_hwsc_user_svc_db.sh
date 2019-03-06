60a0300fb26d92d63f09dbe53982dd407fd19323
Lisa Kim <lisakeem@outlook.com>
2019-03-06 11:32:03 -0800

    Delete unused files and renamed bash scripts
    - renamed build-mongodb-test-container.sh to build_mongodb_test_container.sh

--- db-local-run/run_hwsc_user_svc_db.sh
+++ db-local-run/run_hwsc_user_svc_db.sh
@@ -8,7 +8,8 @@ PORT=$hosts_postgres_port
 USER=$hosts_postgres_user
 SSLMODE=$hosts_postgres_sslmode
 
-MIGRATION_PATH=../db-migrations/hwsc-user-svc
+MIGRATION_PATH=../db-migrations/hwsc-user-svc/test/psql/
+DATA_SEED_FILE_PATH=../backup/unit-test/dev-user/
 POSTGRES_URI=postgres://$USER:$PASSWORD@$HOST:$PORT/$DB?sslmode=$SSLMODE
 
 echo "Launching a local postgres container for hwsc-user-svc:"
@@ -17,22 +18,14 @@ CONTAINER_ID=$(
     --rm \
     -p 5432:5432 \
     -e POSTGRES_PASSWORD=$PASSWORD -e POSTGRES_DB=$DB \
-    --mount type=bind,source="$(pwd)"/$MIGRATION_PATH,target=/test_files \
+    --mount type=bind,source="$(pwd)"/$DATA_SEED_FILE_PATH,target=/test_files \
     postgres:alpine
     )
 echo $CONTAINER_ID
 sleep 3;
 echo
 echo "Running all psql migration files:"
-migrate -path $MIGRATION_PATH/test/psql/ -database $POSTGRES_URI up
-echo
-echo "Seeding Test Data:"
-docker exec -it $CONTAINER_ID psql -U $USER $DB -f /test_files/seed_data_integration_test.sql
-echo
-echo "Displaying Seeded user_svc.accounts"
-docker exec -it $CONTAINER_ID psql -U $USER $DB -c "\x" -c "TABLE user_svc.accounts;";
-echo "Displaying Seeded user_svc.email_tokens"
-docker exec -it $CONTAINER_ID psql -U $USER $DB -c "\x" -c "TABLE user_svc.email_tokens;";
+migrate -path $MIGRATION_PATH -database $POSTGRES_URI up
 echo
 read -p "Press ENTER to stop Postgres container for hwsc-user-svc:"
 docker stop $CONTAINER_ID
--- db-migrations/hwsc-user-svc/seed_data_integration_test.sql
+++
@@ -1,9 +0,0 @@
-INSERT INTO user_svc.accounts
-  (uuid, first_name, last_name, email, password, organization, created_timestamp, is_verified, permission_level)
-VALUES
-('0000xsnjg0mqjhbf4qx1efd6y3', 'IntegrationTest', 'TestVerifyAuthToken', 'vat@test.com', 'pass', 'uwb', current_timestamp, FALSE, 'USER_REGISTRATION');
-
-INSERT INTO user_svc.email_tokens
-  (token, created_timestamp, expiration_timestamp, uuid)
-VALUES
-('mXRLhKATqLq0-5mQ4O9iUob9W896uUhXBT9JrkWMpNs=', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP + INTERVAL '14 day', '0000xsnjg0mqjhbf4qx1efd6y3');
