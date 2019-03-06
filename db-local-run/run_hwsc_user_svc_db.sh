67bfedf6115ee79e6ac56c9c4679a403becc96bf
Lisa Kim <lisakeem@outlook.com>
2019-03-06 09:05:16 -0800

    Update user-svc db-local script and add data seed file

--- db-local-run/run_hwsc_user_svc_db.sh
+++ db-local-run/run_hwsc_user_svc_db.sh
@@ -8,16 +8,31 @@ PORT=$hosts_postgres_port
 USER=$hosts_postgres_user
 SSLMODE=$hosts_postgres_sslmode
 
-MIGRATION_PATH=../db-migrations/hwsc-user-svc/test/psql/
+MIGRATION_PATH=../db-migrations/hwsc-user-svc
 POSTGRES_URI=postgres://$USER:$PASSWORD@$HOST:$PORT/$DB?sslmode=$SSLMODE
 
 echo "Launching a local postgres container for hwsc-user-svc:"
-CONTAINER_ID=$(docker run -d -p 5432:5432 --rm -e POSTGRES_PASSWORD=$PASSWORD -e POSTGRES_DB=$DB postgres:alpine)
+CONTAINER_ID=$(
+    docker run -d \
+    --rm \
+    -p 5432:5432 \
+    -e POSTGRES_PASSWORD=$PASSWORD -e POSTGRES_DB=$DB \
+    --mount type=bind,source="$(pwd)"/$MIGRATION_PATH,target=/test_files \
+    postgres:alpine
+    )
 echo $CONTAINER_ID
 sleep 3;
 echo
 echo "Running all psql migration files:"
-migrate -path $MIGRATION_PATH -database $POSTGRES_URI up
+migrate -path $MIGRATION_PATH/test/psql/ -database $POSTGRES_URI up
 echo
-read -p "Press ENTER to stop and remove Postgres container for hwsc-user-svc:"
+echo "Seeding Test Data:"
+docker exec -it $CONTAINER_ID psql -U $USER $DB -f /test_files/seed_data_integration_test.sql
+echo
+echo "Displaying Seeded user_svc.accounts"
+docker exec -it $CONTAINER_ID psql -U $USER $DB -c "\x" -c "TABLE user_svc.accounts;";
+echo "Displaying Seeded user_svc.email_tokens"
+docker exec -it $CONTAINER_ID psql -U $USER $DB -c "\x" -c "TABLE user_svc.email_tokens;";
+echo
+read -p "Press ENTER to stop Postgres container for hwsc-user-svc:"
 docker stop $CONTAINER_ID
