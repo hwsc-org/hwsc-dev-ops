09cd6dd219243482359f85eb93f8ebd986af74f5
Lisa Kim <lisakeem@outlook.com>
2019-05-17 08:18:10 -0700

    Include user/pass env in user-svc postgres docker file (#28)

--- dockerfiles/hwsc-user-svc/build_push_psql_image.sh
+++ dockerfiles/hwsc-user-svc/build_push_psql_image.sh
@@ -5,7 +5,11 @@ cat ../../db-migrations/hwsc-user-svc/test/psql/*.up.sql > user_svc_schema.sql
 
 # build an image from a docker file, -t = name and optionally a tag in the 'name:tag' format
 # docker build [OPTIONS] PATH
-docker build --build-arg db_name=$hosts_postgres_db -t hwsc/hwsc-user-svc-psql:test-int .
+docker build \
+--build-arg user=$hosts_postgres_user \
+--build-arg password=$hosts_postgres_password \
+--build-arg db_name=$hosts_postgres_db \
+-t hwsc/hwsc-user-svc-psql:test-int .
 
 # docker login
 docker login -u $docker_user --password $docker_password
